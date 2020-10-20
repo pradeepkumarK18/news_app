1038a2ec4143f85ce2a567dfe7c0ddc7c14ff238
pradeepkumarK18 <pradeepsiet@gmail.com>
2020-10-20 15:00:03 +0530

    Update newsApi.js

--- helpers/newsApi.js
+++ helpers/newsApi.js
@@ -1,4 +1,4 @@
 const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('2da91faa1ded4851b4f5f0f1c3a57c5e');
+const newsapi = new NewsAPI('2da91faa1ded4851b4f5f0f1c3a57c5e', { corsProxyUrl: 'https://cors-anywhere.herokuapp.com/' });
 
 export default newsapi;
